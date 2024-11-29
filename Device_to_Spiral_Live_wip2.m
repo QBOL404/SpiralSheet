6ca61dee378246d036e02a2c76b377281ac24c69
ksw2751 <sangwoo7755@gmail.com>
2024-11-29 14:03:37 +0900

    ksw 1129 add title

--- Device_to_Spiral_Live_wip2.m
+++ Device_to_Spiral_Live_wip2.m
@@ -1,30 +1,37 @@
 clear;
 clc;
-close;
+close all force
 clf;
 
 % GUI part
-gui_fig = uifigure;
+function bool = click()
+    bool = true;
+end
 
-update = uibutton(gui_fig, "state", "Text", "Update", "Position", [340 10 100 50]);
+gui_fig = uifigure("Name", "Control Panel");
 
-north_note = uidropdown(gui_fig);
+update = uibutton(gui_fig, "state", "Text", "Update", "Position", [340 10 100 50], "FontSize", 20, "FontColor", 'r');
+
+north_group = uibuttongroup(gui_fig, "Title", "Control Pitch", "Position", [10 180 120 70], "FontSize", 18);
+north_note = uidropdown(north_group, "FontSize", 15,"Tooltip","text");
 north_note.Items = ["C","B","A#","A","G#","G","F#","F","E","D#","D","C#"];
-north_note.Position = [10 180 80 30];
+north_note.Position = [20 5 80 30];
 north_note.Value = "A";
 
-octave_slide = uislider(gui_fig, "range", "Position", [20, 300, 200, 3], "Limits", [0, 8]);
+slide_group = uibuttongroup(gui_fig, "Title", "Octave range", "Position", [20, 300, 240, 90], "FontSize", 18);
+octave_slide = uislider(slide_group, "range", "Position", [15, 40, 200, 3], "Limits", [0, 8], "FontSize", 18);
 octave_slide.MajorTicks = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 octave_slide.MinorTicks = [];
 
-direction_gui = uibuttongroup(gui_fig, "Title", 'Direction', "Position",[170 10 150 130]);
-direction_b1 = uiradiobutton(direction_gui,"Text","Top->Down","Position",[10 20 130 30]);
-direction_b2 = uiradiobutton(direction_gui,"Text","Down->Top","Position",[10 60 130 30]);
+direction_gui = uibuttongroup(gui_fig, "Title", 'Direction', "Position",[170 10 150 130], "FontSize", 18);
+direction_b1 = uiradiobutton(direction_gui,"Text","Top->Down","Position",[10 20 130 30], "FontSize", 18);
+direction_b2 = uiradiobutton(direction_gui,"Text","Down->Top","Position",[10 60 130 30], "FontSize", 18);
 
-scale_gui = uibuttongroup(gui_fig, "Title", 'Scale', "Position",[10 10 150 130]);
-scale_b1 = uiradiobutton(scale_gui,"Text","Linear","Position",[10 20 130 30]);
-scale_b2 = uiradiobutton(scale_gui,"Text","Log","Position",[10 60 130 30]);
+scale_gui = uibuttongroup(gui_fig, "Title", 'Scale', "Position",[10 10 150 130], "FontSize", 18);
+scale_b1 = uiradiobutton(scale_gui,"Text","Linear","Position",[10 20 130 30], "FontSize", 18);
+scale_b2 = uiradiobutton(scale_gui,"Text","Log","Position",[10 60 130 30], "FontSize", 18);
 % GUI part
+
 
 % velocity 업데이트 함수.
 function vel = update_velocity(x)
@@ -36,40 +43,43 @@ function vel = update_velocity(x)
 end
 
 % plot parameter 업데이트 함수.
-function [theta_P, y] = dev(N, scale)
+function [theta_P, y, l_theta_P, l_y, P] = dev(N, scale)
     r = 2^(1/12);
     
-    f_i = 33; % initial freq 
-    note_i = -4; % initial note
-    f = f_i*r.^(-32+note_i:N-2);
-    theta = pi/2 - 2*pi*log2(f/f_i);
-    A = 1./f;
+    f_i = 55; % initial freq 
+    note_i = 33; % initial note
     
     % parted spiral line
-    P = 1;
+    P = 5;
     
-    f_P = f_i*r.^(-32+note_i:1/P:N-2);
+    f_P = f_i*r.^(-note_i+1:1:N-2);
     theta_P = pi/2 - 2*pi*log2(f_P/f_i);
+    
+    l_f_P = f_i*r.^(-note_i+1:1/P:N-2);
+    l_theta_P = pi/2 - 2*pi*log2(l_f_P/f_i);
 
     if scale == 0
         y = 1./f_P;
+        l_y = 1./l_f_P;
         return
     else
         y = log2(1./f_P);
+        l_y = log2(1./l_f_P);
         return
     end
 end
 
+
+
 % 나선악보를 그리기 위한 기초 변수 선언.
 N = 12*8+12;
-[theta_P, log_R_P] = dev(N, 0); % type: linear=0, log=1
-midi_note_i = 20; % -- 이렇게 해버리면 index가 남긴 한데 
+[theta_P, log_R_P, l_theta_P, l_log_R_P, P] = dev(N, 0); % type: linear=0, log=1
 % 나선악보를 그리기 위한 기초 변수 선언
 
 n_start = 1 + 12*round(octave_slide.Value(1))+12;
 n_end = 12*round(octave_slide.Value(2))+12;
 
-s = polarplot(theta_P(n_start:n_end), log_R_P(n_start:n_end), 'b-'); % 추가
+s = polarplot(l_theta_P(P*n_start-16:P*n_end+13-16), l_log_R_P(P*n_start-16:P*n_end+13-16), 'b-'); % 추가
 
 R = log_R_P(1); % 추가
 
@@ -98,10 +108,10 @@ note_on 신호의 velocity가 0일 때 note_off를 표현함.
 off note 신호가 있는 경우: false로 설정
 off note 신호가 없는 경우: true로 설정
 %}
-only_on_signal = false;
+only_on_signal = true;
 % midi device를 device 변수에 할당.
 mididevinfo
-device = mididevice(1);
+device = mididevice(0);
 
 note_list = zeros(1, N); % index: note number, 값이 1일 경우 켜진 note / 0일 경우 꺼진 note.
 velocity_list = ones(1, N); % 각 note의 velocity 값 저장.
@@ -118,31 +128,38 @@ while 1
             hl(i).Visible = 'off';
         end
         N = 12*8;
-        [theta_P, log_R_P] = dev(N, scale_b2.Value); % type: linear=0, log=1
-        midi_note_i = 20; % -- 이렇게 해버리면 index가 남긴 한데 
+        [theta_P, log_R_P, l_theta_P, l_log_R_P, P] = dev(N, scale_b2.Value); % type: linear=0, log=1
+        
         n_start = 1 + 12*round(octave_slide.Value(1))+12;
         n_end = 12*round(octave_slide.Value(2))+12;
-        s.XData = theta_P(n_start:n_end);
-        s.YData = log_R_P(n_start:n_end);
+        
 
         n_n = find(strcmp(note_label, north_note.Value), 2);
         n_n = n_n(2);
-        set(gca,'thetaticklabel', note_label(n_n-3:9+n_n));
-        n_n = n_n-16;
-        if n_start < n_end
-            rlim([min(log_R_P(n_start:end)) max(log_R_P(n_start:end))]);
-        end
+               
         if direction_b2.Value == 1
+            set(gca,'thetaticklabel', flip(note_label(n_n-8:3+n_n)));
+            if n_start < n_end
+                rlim([min(log_R_P(n_start+n_n-16:n_end+n_n-16)) max(log_R_P(n_start+n_n-16:n_end+n_n-16))]);
+            end
+            s.XData = l_theta_P(P*n_start+n_n-16:P*n_end+n_n-16);
+            s.YData = l_log_R_P(P*n_start+n_n-16:P*n_end+n_n-16);
             for i=1:N
-                ho(i)=polarplot(theta_P(N-i+1),log_R_P(N-i+1),'ro','MarkerFaceColor',[1 .6 .6],'MarkerSize',10);
-                hl(i)=polarplot([theta_P(i) theta_P(N-i+1)],[min(log_R_P) log_R_P(N-i+1)],'r-', LineWidth=1.5);
+                ho(i)=polarplot(theta_P(127+3-(i+n_n)),log_R_P(127+3-(i+n_n)),'ro','MarkerFaceColor',[1 .6 .6],'MarkerSize',10);
+                hl(i)=polarplot([theta_P(127+3-(i+n_n)) theta_P(127+3-(i+n_n))],[min(log_R_P(n_start+n_n-16:n_end+n_n-16)) log_R_P(127+3-(i+n_n))],'r-', LineWidth=1.5);
                 ho(i).Visible = 'off';
                 hl(i).Visible = 'off';
             end
         else
+            set(gca,'thetaticklabel', note_label(n_n-3:8+n_n));
+            if n_start < n_end
+                rlim([min(log_R_P(n_start+n_n-16:n_end+n_n-16)) max(log_R_P(n_start+n_n-16:n_end+n_n-16))]);
+            end
+            s.XData = l_theta_P(P*n_start+n_n-16:P*n_end+n_n-16);
+            s.YData = l_log_R_P(P*n_start+n_n-16:P*n_end+n_n-16);
             for i=1:N
-                ho(i)=polarplot(theta_P(i+12+n_n),log_R_P(i+12+n_n),'ro','MarkerFaceColor',[1 .6 .6],'MarkerSize',10);
-                hl(i)=polarplot([theta_P(i+12+n_n) theta_P(i+12+n_n)],[min(log_R_P) log_R_P(i+12+n_n)],'r-', LineWidth=1.5);
+                ho(i)=polarplot(theta_P(i-4+n_n),log_R_P(i-4+n_n),'ro','MarkerFaceColor',[1 .6 .6],'MarkerSize',10);
+                hl(i)=polarplot([theta_P(i-4+n_n) theta_P(i-4+n_n)],[min(log_R_P(n_start+n_n-16:n_end+n_n-16)) log_R_P(i-4+n_n)],'r-', LineWidth=1.5);
                 ho(i).Visible = 'off';
                 hl(i).Visible = 'off';
             end
@@ -155,7 +172,12 @@ while 1
     % GUI part
 
     msgArray = midireceive(device); % midi device에서 midi signal을 받아옴.
-    %msgArray = [];
+
+    % Print midi
+    if length(msgArray)>0
+        msgArray
+    end
+
     if isempty(msgArray) % 입력이 없을 경우velocity만 업데이트.
         velocity_list = arrayfun(@update_velocity, velocity_list);
         vel = num2cell(velocity_list);
@@ -165,12 +187,15 @@ while 1
         continue
     end
     
-    note_list([msgArray([msgArray.Type] == 1).Note]-midi_note_i) = 1; % signal의 note number에 따라 index 값을 1로 변경.
-    CC_signal = msgArray([msgArray.Type] == 12); % pedal signal만 필터링.
-    note_signal = msgArray([msgArray.Type] == 1); % CC 신호와 아예 분리로 수정. > 속도면에서 단점이 있으면 
+    % signal의 note number에 따라 index 값을 1로 변경.
+    note_list([msgArray([msgArray.Type] == 1).Note]) = 1; 
+    CC_signal = msgArray([msgArray.Type] == 12); 
+    note_signal = msgArray([msgArray.Type] == 1); 
+
+    % CC 신호와 아예 분리로 수정. > 속도면에서 단점이 있으면 
 
     % -- pedal catch
-    if length(CC_signal)==1 % 에러 방지를 막고자, 길이가 1일때만 취급. 
+    if length(CC_signal)==1 % 길이가 1일때만 
         % CCsignal
         if CC_signal.CCNumber == 64 & CC_signal.CCValue > 0
             disp("pedal on")
@@ -183,29 +208,19 @@ while 1
         end
     end
     
-    % -- velocity control
-    
+    % -- velocity control 
     if pedal_signal == false
-        %velocity_list([msgArray([msgArray.Type] == 1).Note]) = [msgArray([msgArray.Type] == 1).Velocity];
-        velocity_list([note_signal.Note]-midi_note_i) = [note_signal.Velocity];
+        velocity_list([note_signal.Note]) = [note_signal.Velocity];
     else
-        % pedal singal이 있을 경우 note 정보만 받아오기 위한 필터링이 필요함.
-        %velocity_list([msgArray([msgArray([msgArray([msgArray.Type] == 1).Velocity] ~= 0).Type] == 1).Note]) = [msgArray([msgArray([msgArray([msgArray.Type] == 1).Velocity] ~= 0).Type] == 1).Velocity];
-        velocity_list([note_signal([note_signal.Velocity]~=0).Note]-midi_note_i) = [note_signal([note_signal.Velocity] ~= 0).Velocity];
+       velocity_list([note_signal([note_signal.Velocity]~=0).Note]) = [note_signal([note_signal.Velocity] ~= 0).Velocity];
     end
 
     % note_off 신호 존재에 따른 note 업데이트.
     if ~only_on_signal
-        note_list([msgArray([msgArray.Type] == 2).Note]-midi_note_i) = 0; % -- 에러 발생 가능 코드 
+        note_list([msgArray([msgArray.Type] == 2).Note]) = 0; % -- 에러 발생 가능 코드 
     else
-        % -- 리스트 확인 코드 
-        % if length(msgArray([msgArray.Type] == 1))>0
-        %     msgArray([msgArray.Type] == 1);
-        % end
-        % -- 
 
-        note_list([note_signal([note_signal.Velocity] == 0).Note]-midi_note_i) = 0;
-        %note_list([msgArray([msgArray([msgArray.Type] == 1).Velocity] == 0).Note]) = 0;
+        note_list([note_signal([note_signal.Velocity] == 0).Note]) = 0;
     end
     
     %{
@@ -228,7 +243,7 @@ while 1
         set(ho(note_list == 0),'visible','off');
     end
     % 잔음 노트의 색을 파란색으로 변경.
-    set(ho(note_list == 0), 'MarkerFaceColor','blue');
+    set(ho(note_list == 0), 'MarkerFaceColor',[1, .8, .8]);
     set(hl(note_list == 0),'visible','off');
 
     drawnow;
