ec0045a9eee170b0f612fa1bebab82e0548f1693
ksw2751 <sangwoo7755@gmail.com>
2024-12-03 03:14:56 +0900

    ksw 1203 add linecolor

--- Device_to_Spiral_Live_wip3.m
+++ Device_to_Spiral_Live_wip3.m
@@ -4,32 +4,41 @@ close all force
 clf;
 
 % GUI part
-function bool = click()
-    bool = true;
+%{
+function openLink(~, ~)
+    url = 'https://sites.google.com/view/qbolab';
+    web(url, '-browser');
 end
+%}
+gui_fig = uifigure("Name", "Control Panel", "Position", [1500, 600, 560, 400]);
 
-gui_fig = uifigure("Name", "Control Panel");
-
-update = uibutton(gui_fig, "state", "Text", "Update", "Position", [340 10 100 50], "FontSize", 20, "FontColor", 'r', "Tooltip", "Update changed settings");
+update = uibutton(gui_fig, "state", "Text", "Apply", "Position", [450 10 100 50], "FontSize", 20, "FontColor", 'r');
+update.Tooltip = "Apply the modified settings.";
 
 north_group = uibuttongroup(gui_fig, "Title", "Control Pitch", "Position", [10 180 120 70], "FontSize", 18);
-north_note = uidropdown(north_group, "FontSize", 15,"Tooltip","text");
+north_group.Tooltip = "Change the note displayed at the 12 o'clock position.";
+north_note = uidropdown(north_group, "FontSize", 15);
 north_note.Items = ["C","B","A#","A","G#","G","F#","F","E","D#","D","C#"];
 north_note.Position = [20 5 80 30];
 north_note.Value = "A";
 
-slide_group = uibuttongroup(gui_fig, "Title", "Octave range", "Position", [20, 300, 240, 90], "FontSize", 18);
+slide_group = uibuttongroup(gui_fig, "Title", "Octave range", "Position", [10, 300, 240, 90], "FontSize", 18);
+slide_group.Tooltip = "Adjust the octave display range.";
 octave_slide = uislider(slide_group, "range", "Position", [15, 40, 200, 3], "Limits", [0, 8], "FontSize", 18);
 octave_slide.MajorTicks = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 octave_slide.MinorTicks = [];
 
 direction_gui = uibuttongroup(gui_fig, "Title", 'Direction', "Position",[170 10 150 130], "FontSize", 18);
+direction_gui.Tooltip = "Change the direction in which the pitch increases.";
 direction_b1 = uiradiobutton(direction_gui,"Text","Top->Down","Position",[10 20 130 30], "FontSize", 18);
 direction_b2 = uiradiobutton(direction_gui,"Text","Down->Top","Position",[10 60 130 30], "FontSize", 18);
 
 scale_gui = uibuttongroup(gui_fig, "Title", 'Scale', "Position",[10 10 150 130], "FontSize", 18);
+scale_gui.Tooltip = "Change the scale of the notes.";
 scale_b1 = uiradiobutton(scale_gui,"Text","Linear","Position",[10 20 130 30], "FontSize", 18);
 scale_b2 = uiradiobutton(scale_gui,"Text","Log","Position",[10 60 130 30], "FontSize", 18);
+
+%homepage = uicontrol(gui_fig, "Tooltip", "L",  "Position", [500, 10, 20, 20], 'Callback', @openLink);
 % GUI part
 
 
@@ -42,7 +51,7 @@ function vel = update_velocity(x)
     end
 end
 
-% plot parameter 업데이트 함수.
+% plot parameter 업데이트 함수. 노트 개수(N)와 scale을 
 function [theta_P, y, l_theta_P, l_y, P] = dev(N, scale)
     r = 2^(1/12);
     
@@ -69,17 +78,17 @@ function [theta_P, y, l_theta_P, l_y, P] = dev(N, scale)
     end
 end
 
-
-
 % 나선악보를 그리기 위한 기초 변수 선언.
 N = 12*8+12;
 [theta_P, log_R_P, l_theta_P, l_log_R_P, P] = dev(N, 0); % type: linear=0, log=1
-% 나선악보를 그리기 위한 기초 변수 선언
 
 n_start = 1 + 12*round(octave_slide.Value(1))+12;
-n_end = 12*round(octave_slide.Value(2))+12;
 
-s = polarplot(l_theta_P(P*n_start-16:P*n_end+13-16), l_log_R_P(P*n_start-16:P*n_end+13-16), 'b-'); % 추가
+r = linspace(0, 1, length(l_log_R_P)).^(0.5);
+r = (r - min(r)) / (max(r) - min(r));
+line_color = 1 - r;
+
+s_base = polarplot(l_theta_P, l_log_R_P, 'Color', [0, 0, 0, 0]);
 
 R = log_R_P(1); % 추가
 
@@ -91,8 +100,14 @@ rlim([min(log_R_P(n_start:end)) max(log_R_P(n_start:end))]);
 set(gca,'thetaticklabel', note_label(1:12));
 set(gca,'rticklabel',[]);
 set(gcf,'position',[300,150,800,800]);
+set(gcf, "Name", "Spiral Sheet")
 hold on
 
+% 배경 라인 객체 생성.
+for i=1:length(l_theta_P)-1
+    s(i) = polarplot(l_theta_P(i:i+1), l_log_R_P(i:i+1), 'Color', [line_color(i), 0,1 - line_color(i)]);
+    s(i).Visible = 'on';
+end
 
 % 각 plot point 객체 생성
 for i=1:N
@@ -127,23 +142,33 @@ while 1
             ho(i).Visible = 'off';
             hl(i).Visible = 'off';
         end
+        for i=1:length(l_theta_P)-1
+            s(i).Visible = 'off';
+        end
         N = 12*8;
         [theta_P, log_R_P, l_theta_P, l_log_R_P, P] = dev(N, scale_b2.Value); % type: linear=0, log=1
         
         n_start = 1 + 12*round(octave_slide.Value(1))+12;
         n_end = 12*round(octave_slide.Value(2))+12;
-        
 
         n_n = find(strcmp(note_label, north_note.Value), 2);
         n_n = n_n(2);
-               
+        
+        plot_theta = l_theta_P(P*n_start+n_n-16:P*n_end+n_n-16);
+        plot_log = l_log_R_P(P*n_start+n_n-16:P*n_end+n_n-16);
+        r = linspace(0, 1, length(plot_log)).^(0.5);
+        r = (r - min(r)) / (max(r) - min(r));
+        line_color = 1 - r;
+
         if direction_b2.Value == 1
             set(gca,'thetaticklabel', flip(note_label(n_n-8:3+n_n)));
             if n_start < n_end
-                rlim([min(log_R_P(n_start+n_n-16:n_end+n_n-16)) max(log_R_P(n_start+n_n-16:n_end+n_n-16))]);
+                rlim([min(plot_log(:)) max(plot_log(:))]);
             end
-            s.XData = l_theta_P(P*n_start+n_n-16:P*n_end+n_n-16);
-            s.YData = l_log_R_P(P*n_start+n_n-16:P*n_end+n_n-16);
+            for i=1:length(plot_log)-1
+                s(i) = polarplot(plot_theta(i:i+1), plot_log(i:i+1), 'Color', [1 - line_color(i), 0, line_color(i)]);
+                s(i).Visible = 'on';
+            end
             for i=1:N
                 ho(i)=polarplot(theta_P(127+3-(i+n_n)),log_R_P(127+3-(i+n_n)),'ro','MarkerFaceColor',[1 .6 .6],'MarkerSize',10);
                 hl(i)=polarplot([theta_P(127+3-(i+n_n)) theta_P(127+3-(i+n_n))],[min(log_R_P(n_start+n_n-16:n_end+n_n-16)) log_R_P(127+3-(i+n_n))],'r-', LineWidth=1.5);
@@ -153,10 +178,12 @@ while 1
         else
             set(gca,'thetaticklabel', note_label(n_n-3:8+n_n));
             if n_start < n_end
-                rlim([min(log_R_P(n_start+n_n-16:n_end+n_n-16)) max(log_R_P(n_start+n_n-16:n_end+n_n-16))]);
+                rlim([min(plot_log(:)) max(plot_log(:))]);
             end
-            s.XData = l_theta_P(P*n_start+n_n-16:P*n_end+n_n-16);
-            s.YData = l_log_R_P(P*n_start+n_n-16:P*n_end+n_n-16);
+            for i=1:length(plot_log)-1
+                s(i) = polarplot(plot_theta(i:i+1), plot_log(i:i+1), 'Color', [line_color(i), 0,1 - line_color(i)]);
+                s(i).Visible = 'on';
+            end
             for i=1:N
                 ho(i)=polarplot(theta_P(i-4+n_n),log_R_P(i-4+n_n),'ro','MarkerFaceColor',[1 .6 .6],'MarkerSize',10);
                 hl(i)=polarplot([theta_P(i-4+n_n) theta_P(i-4+n_n)],[min(log_R_P(n_start+n_n-16:n_end+n_n-16)) log_R_P(i-4+n_n)],'r-', LineWidth=1.5);
@@ -164,15 +191,14 @@ while 1
                 hl(i).Visible = 'off';
             end
         end
-
         update.Value = false;
         disp("Finish");
     end
     
     % GUI part
 
-    msgArray = midireceive(device); % midi device에서 midi signal을 받아옴.
-
+    %msgArray = midireceive(device); % midi device에서 midi signal을 받아옴.
+    msgArray = [];
     % Print midi
     if length(msgArray)>0
         msgArray
